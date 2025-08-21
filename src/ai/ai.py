62fb412fb762ea69642593d57f2538443b864a3e
Wesley Xavier <wesley.xvier@gmail.com>
2025-08-21 09:29:17 -0300

    :sparkles: feat: atualizaçao de interface de i.a. - troca do modelo por gemma - criação de modelo especializado

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -1,28 +1,47 @@
-from ollama import chat
-from ollama import ChatResponse
+import logging
+from ollama import Client
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG,
+    encoding='utf-8'
+)
 
 _PROMPT = """
-Você é um gerador de scripts sql, a base de dados
-padrão se chama 'people' e só tem um campo 'name',
-estamos usando sqlite no python,
-você receberá um pedido na forma de texto,
-traduza esse pedido em query,
-mostre apenas a query sem textos nem comentários,
-remova as marcas de markdown e mostre o texto puro
-segue o pedido: """
+You are a SQL script generator.
+The default database is named 'produtos' and has two columns:
+'name' and 'quantity'.
+We are using SQLite3 in Python.
+You will receive a request in text format.
+Translate this request into a query.
+Make query correct and optimizeds.
+Show only the query without any text, comments, or Markdown.
+The output should be plain text. """
+
+client = Client()
+
+client.create(
+    model='db-assistent',
+    from_='gemma3',
+    system='You are a SQL script generator.',
+    stream=False
+)
 
 
 def get_query(msg: str) -> str | None:
     if msg is None or '':
         return None
-    response: ChatResponse = chat(model='qwen3:4b', messages=[
-        {
-            'role': 'user',
-            'content': _PROMPT + msg,
-        },
-    ])
-    return response.message.content
+    result = client.generate(model='db-assistent',
+                             prompt=_PROMPT+msg)['response']
+    logging.info(result)
+    return result
 
+
+if __name__ == '__main__':
+    result = get_query(input('teste: '))
+    # if result is not None:
+    #     result = result.split('</think>')[1].strip()
+    print(result)
 
 # print(response['message']['content'])
 # or access fields directly from the response object
